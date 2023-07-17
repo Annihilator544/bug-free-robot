8cf4a2f4a2f2c7ade7ee58c3b6ebf77e13313890
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2023-07-17 10:55:31 +0530

    Add files via upload

---
+++ C#/#1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Class1
+{
+	int multiplesof3and5(int i)
+	{
+		int sum = 0;
+		for (int j = 0; j < i; j++)
+		{
+			if (j % 3 == 0)
+			{
+				sum+=j;
+			}
+			if(j % 5 == 0)
+			{
+				sum += j;
+			}
+			if (j % 15 == 0)
+			{
+				sum-= j;
+			}
+		
+			return sum;
+	}
+	public static void main(string[]args)
+	{
+			multiplesof3and5(49);
+			multiplesof3and5(1000);
+			multiplesof3and5(8456);
+	}
+}
